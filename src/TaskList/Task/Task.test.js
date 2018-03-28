47cb2c5effba66efa98cc9c5c807359ae9ec3e9c
EsNFish <esnfish@gmail.com>
2018-03-28 15:18:28 -0500

    Finished task unit tests

--- src/TaskList/Task/Task.test.js
+++ src/TaskList/Task/Task.test.js
@@ -8,10 +8,16 @@ import moment from 'moment';
 
 Enzyme.configure({adapter: new Adapter()})
 describe('Task', () =>{
-    it('creates a proper task', () =>{
-        const date = new Date();
-        const newTaskKey = 1234;
-        const task = {
+
+    let date;
+    let newTaskKey;
+    let task;
+    let component;
+
+    beforeEach(() => {
+         date = new Date();
+         newTaskKey = 1234;
+         task = {
             key: 1789,
             priority : 1,
             todo : "bob",
@@ -19,12 +25,28 @@ describe('Task', () =>{
             dateDue : date,
             status : 'completed'
         }
-        const component = mount(<Task key={newTaskKey} task={task}/>);
+         component = shallow(<Task key={newTaskKey} task={task}/>);
+    });
+
+    it('has a tr', ()  => {
+        expect(component.find('tr').length).equal(1);
+    });
+
+    it('has a tr with 5 tds', () => {
+        expect((component.find('tr').at(0)).find('td').length).equal(5);
+    });
+
+    it('has a td of length 5', () => {
         expect(component.find('td').length).equal(5);
+    });
+
+    it('has the proper values', () =>{
         expect(component.find('td').at(0).text()).to.equal('1');
         expect(component.find('td').at(1).text()).to.equal('bob');
         expect(component.find('td').at(2).text()).to.equal(moment(date).format("Do MMM YYYY"));
         expect(component.find('td').at(3).text()).to.equal(moment(date).format("Do MMM YYYY"));
         expect(component.find('td').at(4).text()).to.equal('completed');
     });
+
+    
 })
