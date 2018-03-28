d48ce4afbe0a19dc4f80e3a52ff5a9c3481c2da7
EsNFish <esnfish@gmail.com>
2018-03-28 14:21:32 -0500

    finished up Task unit tests

--- src/TaskList/Task/Task.test.js
+++ src/TaskList/Task/Task.test.js
@@ -1,31 +1,30 @@
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-// import {renderComponent, expect} from './test_helper';
 import {shallow, mount} from 'enzyme';
 import {Task} from './Task';
-// import {TaskList} from './TaskList/TaskList';
 import React from 'react';
 import {expect} from 'chai';
+import moment from 'moment';
 
 Enzyme.configure({adapter: new Adapter()})
 describe('Task', () =>{
-    it('renders a thing', () =>{
+    it('creates a proper task', () =>{
+        const date = new Date();
         const newTaskKey = 1234;
         const task = {
             key: 1789,
             priority : 1,
             todo : "bob",
-            dateEntered : new Date(),
-            dateDue : new Date(),
+            dateEntered : date,
+            dateDue : date,
             status : 'completed'
         }
         const component = mount(<Task key={newTaskKey} task={task}/>);
-        const date = new Date();
-        // componet.setProps({task, newTaskKey});
+        expect(component.find('td').length).equal(5);
         expect(component.find('td').at(0).text()).to.equal('1');
         expect(component.find('td').at(1).text()).to.equal('bob');
-        // expect(component.find('td').at(2).text()).to.equal({date});
-        // expect(component.find('td').at(3).text()).to.equal({date});
+        expect(component.find('td').at(2).text()).to.equal(moment(date).format("Do MMM YYYY"));
+        expect(component.find('td').at(3).text()).to.equal(moment(date).format("Do MMM YYYY"));
         expect(component.find('td').at(4).text()).to.equal('completed');
     });
 })
