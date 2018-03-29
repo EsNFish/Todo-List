94861f0469b455c30815035d92ad61a40a3dc67b
EsNFish <esnfish@gmail.com>
2018-03-29 10:34:30 -0500

    Edit task unit test except for due date drop down, couldn't figure it out yet

--- src/TaskList/EditTask/EditTasks.test.js
+++ src/TaskList/EditTask/EditTasks.test.js
@@ -5,6 +5,8 @@ import {EditTask} from './EditTask';
 import React from 'react';
 import {expect} from 'chai';
 import moment from 'moment';
+import {FormControl} from 'react-bootstrap';
+import {Component} from 'react';
 
 Enzyme.configure({adapter: new Adapter()})
 describe('EditTask', () =>{
@@ -41,13 +43,15 @@ describe('EditTask', () =>{
     });
 
     it('has the proper values', () =>{
-        // console.log(component.debug());
-        // expect(((component.find('tr').at(0)).find('td').at(0)).find('joe'));
-        // expect(component.find('td').at(0).find('FormControl').equal(1));
-        console.log(component.find('FormControl').debug());
-        // expect(component.find('td').at(2).text()).to.equal(moment(date).format("Do MMM YYYY"));
-        // expect(component.find('td').at(3).text()).to.equal(moment(date).format("Do MMM YYYY"));
-        // expect(component.find('td').at(4).text()).to.equal('completed');
+        expect(component.find(FormControl).at(0).dive().props()).to.have.property('value', 1);
+        expect(component.find('td').at(1).find('ContentEditable').props()).to.have.property('html', "bob");
+        expect(component.find('td').at(2).text()).to.equal(moment(date).format("Do MMM YYYY"));
+        // console.log(component.find('td').at(3).dive().debug());
+        // expect(component.find('td').at(3).dive().props()).to.have.property('value', moment(date).format("Do MMM YYYY"));
+        // expect(component.find('td').at(3).find("dateFormat").props()).to.have.property(moment(date).format("Do MMM YYYY"));
+        expect(component.find(FormControl).at(1).dive().props()).to.have.property('value', 'completed');
+        expect(component.find('td').at(5).find("Button").props()).to.have.property('bsStyle', "success");
+        expect(component.find('td').at(6).find("Button").props()).to.have.property('bsStyle', "danger");
     });
 
     
