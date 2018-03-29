43e9c551766d14142d5168a933b2fd404ba28dd4
EsNFish <esnfish@gmail.com>
2018-03-29 09:54:02 -0500

    Updating edit task

--- src/TaskList/EditTask/EditTasks.test.js
+++ src/TaskList/EditTask/EditTasks.test.js
@@ -1,6 +1,6 @@
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import {shallow, mount} from 'enzyme';
+import {shallow, mount, render} from 'enzyme';
 import {EditTask} from './EditTask';
 import React from 'react';
 import {expect} from 'chai';
@@ -27,7 +27,6 @@ describe('EditTask', () =>{
         }
          component = shallow(<EditTask key={newTaskKey} task={task} isEditable={true}/>);
     });
-    it('Doesn\'t break', () => {});
 
     it('has a tr', ()  => {
         expect(component.find('tr').length).equal(1);
@@ -42,8 +41,10 @@ describe('EditTask', () =>{
     });
 
     it('has the proper values', () =>{
-        expect((component.find('td').at(0)).find('td').length).equal(1);
-        // expect(component.find('td').at(1).text()).to.equal('bob');
+        // console.log(component.debug());
+        // expect(((component.find('tr').at(0)).find('td').at(0)).find('joe'));
+        // expect(component.find('td').at(0).find('FormControl').equal(1));
+        console.log(component.find('FormControl').debug());
         // expect(component.find('td').at(2).text()).to.equal(moment(date).format("Do MMM YYYY"));
         // expect(component.find('td').at(3).text()).to.equal(moment(date).format("Do MMM YYYY"));
         // expect(component.find('td').at(4).text()).to.equal('completed');
